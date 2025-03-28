679b9cd5bfb44eb5226350272ef877d5d4aaf1d2
BenjaminLeo2008 <166539016+BenjaminLeo2008@users.noreply.github.com>
2025-03-28 08:56:02 -0300

    botonfunciona

---
+++ Assets/Scripts/Boton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public GameObject botonGO;
+    private void OnMouseDown()
+    {
+        botonGO.SetActive(false);
+    }
+    private void OnMouseUp()
+    {
+        botonGO.SetActive(true);
+    }
+}
